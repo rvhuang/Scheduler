ea116213a4e490f7f3d9fd28c564bae3418e5d7f
rvhuang <rvhuang@outlook.com>
2017-02-02 22:14:33 +0800

    Added example for observable Redis wrapper

---
+++ src/ModelWorkshop.Example.Redis/Request.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ModelWorkshop.Example.Redis
+{
+    public class Request
+    {
+        public int ProcessId { get; set; }
+
+        public long Timestamp { get; set; }
+        
+        public DateTime Date { get; set; }
+
+        public Guid GUID { get; set; }
+    }
+}
