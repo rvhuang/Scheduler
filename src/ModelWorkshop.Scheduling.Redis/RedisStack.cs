2bf02488a8e23156e4c6d2a768dcb15abed33236
rvhuang <rvhuang@outlook.com>
2017-02-14 09:41:45 +0800

    Added documentation comments

--- src/ModelWorkshop.Scheduling.Redis/RedisStack.cs
+++ src/ModelWorkshop.Scheduling.Redis/RedisStack.cs
@@ -4,15 +4,33 @@ using System.Diagnostics;
 
 namespace ModelWorkshop.Scheduling.Redis
 {
+    /// <summary>
+    /// Represents a first-in, last-out collection of objects stored in Redis.
+    /// </summary>
+    /// <typeparam name="TItem">Specifies the type of elements in the queue.</typeparam>
     public class RedisStack<TItem> : RedisCollectionBase<TItem>
     {
         #region Consturctors
 
+        /// <summary>
+        /// Initializes a new instance of the class with Redis configuration string, key and database index. 
+        /// </summary>
+        /// <param name="configuration">The Redis configuration string.</param>
+        /// <param name="key">The key that is associated with the Redis list.</param>
+        /// <param name="db">The index of database that the list is stored.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
         public RedisStack(string configuration, RedisKey key, int db)
             : base(configuration, key, db)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the class with Redis configuration, key and database index. 
+        /// </summary>
+        /// <param name="configuration">The Redis configuration options.</param>
+        /// <param name="key">The key that is associated with the Redis list.</param>
+        /// <param name="db">The index of database that the list is stored.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
         public RedisStack(ConfigurationOptions configuration, RedisKey key, int db)
             : base(configuration, key, db)
         {
@@ -22,6 +40,11 @@ namespace ModelWorkshop.Scheduling.Redis
 
         #region RedisCollectionBase Methods
 
+        /// <summary>
+        /// Attempts to add an object to the Redis list.
+        /// </summary>
+        /// <param name="item">The object to add to the list.</param>
+        /// <returns>true if the object was added successfully; otherwise, false.</returns>
         public override bool TryAdd(TItem item)
         {
             using (var conn = base.GetConnection())
@@ -39,6 +62,11 @@ namespace ModelWorkshop.Scheduling.Redis
             return true;
         }
 
+        /// <summary>
+        /// Attempts to remove and return the last object from the Redis list.
+        /// </summary>
+        /// <param name="item">When this method returns, if the operation was successful, item contains the object removed. If no object was available to be removed, the value is unspecified.</param>
+        /// <returns>true if an element was removed and returned succesfully; otherwise, false.</returns>
         public override bool TryTake(out TItem item)
         {
             var value = RedisValue.Null;
@@ -66,6 +94,11 @@ namespace ModelWorkshop.Scheduling.Redis
             }
         }
 
+        /// <summary>
+        /// Tries to return the last object from the Redis list without removing it.
+        /// </summary>
+        /// <param name="item">When this method returns, result contains an object from the Redis list or an unspecified value if the operation failed.</param>
+        /// <returns>true if an object was returned successfully; otherwise, false.</returns>
         public override bool TryPeek(out TItem item)
         {
             var value = RedisValue.Null;
